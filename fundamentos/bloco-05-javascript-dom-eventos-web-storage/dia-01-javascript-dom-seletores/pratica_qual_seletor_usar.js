38264c4b9e7e8cc20c70e98a9c94cb91a8446654
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-03-16 22:57:03 -0300

    finalizado exercicios bloco 5 dia 1 com sucesso

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-javascript-dom-seletores/pratica_qual_seletor_usar.js
@@ -0,0 +1,25 @@
+document.getElementById('header-container').style.background = 'green';
+
+let elementSectionLeft = document.getElementsByClassName('emergency-tasks')[0];
+
+elementSectionLeft.style.background = 'green';
+
+let subtitleEmergencyTasks = document.getElementsByTagName('h3');
+
+console.log(subtitleEmergencyTasks);
+
+subtitleEmergencyTasks[0].style.backgroundColor = 'black';
+subtitleEmergencyTasks[1].style.backgroundColor = 'black';
+
+let elementSectionRight = document.getElementsByClassName('no-emergency-tasks')[0];
+
+elementSectionRight.style.background = 'blue';
+
+
+subtitleEmergencyTasks[2].style.background = 'black';
+subtitleEmergencyTasks[3].style.background = 'black';
+
+const elementFooter = document.getElementById('footer-container');
+
+elementFooter.style.background = 'green';
+
