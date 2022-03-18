b48812494ff90e1fd0d37e548c65222b0ec4b476
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-03-18 16:35:17 -0300

    finalizado requisito 8 exercicio calendario

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/exercicio_calendario.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-03-javascript-eventos/exercicio_calendario.js
@@ -169,6 +169,21 @@ getSpanTask[0].innerText = task;
 }
 tarefaPersonalizada(task);
 
+// 8 - Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+const color = 'green'
+
+function corTarefa(color){
+  let elementDivTask = document.createElement('div');
+  let getDivMyTasks = document.getElementsByClassName('my-tasks');
+  getDivMyTasks[0].appendChild(elementDivTask);
+  elementDivTask.classList.add('task');
+  elementDivTask.style.background = color;
+}
+corTarefa(color);
+
   
 
 
