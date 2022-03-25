f3747cc0d6623f9b1c5a60e09db433b59241bae6
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-03-24 21:10:01 -0300

    exercicio formulario

--- fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/prevent_default.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/prevent_default.js
@@ -3,9 +3,21 @@ const INPUT_TEXT = document.querySelector("#input-text");
 const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
 
-
-function inibirComportamento (event) {
-    event.preventDefault();
+function inibirComportamento(event) {
+  event.preventDefault();
 }
 
 HREF_LINK.addEventListener('click', inibirComportamento);
+
+INPUT_CHECKBOX.addEventListener('click', inibirComportamento);
+
+
+function inibirLetraA(event) {
+    const character = event.key;
+    if (character !== 'a') {
+      event.preventDefault();
+    }
+}
+
+INPUT_TEXT.addEventListener('keypress', inibirLetraA);
+
