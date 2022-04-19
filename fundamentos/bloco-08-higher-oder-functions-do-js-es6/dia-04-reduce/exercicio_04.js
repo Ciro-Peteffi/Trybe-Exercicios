6941f8e37c4349678e58840c8373933e1b773069
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-04-19 16:06:41 -0300

    exercicios de fixação bloco 9.1

---
+++ fundamentos/bloco-08-higher-oder-functions-do-js-es6/dia-04-reduce/exercicio_04.js
@@ -0,0 +1,18 @@
+
+const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  };
+
+//   4 - Encontre o livro com o maior nome.
+  
+
+function longestNamedBook() {
+    
+  }
