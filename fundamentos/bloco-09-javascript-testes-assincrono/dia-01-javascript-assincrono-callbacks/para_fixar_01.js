39942c312e100c7099a0309a691aa3f262351c0b
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-04-24 16:11:44 -0300

    inserido exemplo callback

--- fundamentos/bloco-09-javascript-testes-assincrono/dia-01-javascript-assincrono-callbacks/para_fixar_01.js
+++ fundamentos/bloco-09-javascript-testes-assincrono/dia-01-javascript-assincrono-callbacks/para_fixar_01.js
@@ -1,3 +1,11 @@
+// 1 - Adicione uma callback como parâmetro da funcão getUser .
+// No código abaixo você tem a função getUser , que define um objeto com os dados de uma pessoa. Complete a função getUser de forma que ela receba uma função callback como parâmetro para realizar as operações abaixo:
+// Insira o retorno da função getUser ;
+// Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
+// Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian".
+
+
+
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
