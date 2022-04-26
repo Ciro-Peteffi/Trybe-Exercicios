fb89ceac80f8a2014cd26adaecfd84ce96d53f54
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-04-26 00:23:42 -0300

    praticando com api de crypto moedas

--- fundamentos/bloco-09-javascript-testes-assincrono/dia-02-fetch-api-async-await/exercicio_02.js
+++ fundamentos/bloco-09-javascript-testes-assincrono/dia-02-fetch-api-async-await/exercicio_02.js
@@ -1,5 +1,20 @@
-const URL = `https://api.coincap.io/v2/assets`
+const URL = `https://api.coincap.io/v2/assets`;
 
-const fectchCrypto = (crypto) => {
-    
-}
+const listCrypto = document.getElementById("crypto-list");
+
+const fetchCrypto = async () => {
+  await fetch(URL)
+    .then((response) => response.json())
+    .then((crypto) => {
+      crypto.data.forEach((crypto, i) => {
+        if (i < 10) {
+          const li = document.createElement("li");
+          li.innerText = `${crypto.name} ${crypto.symbol}: ${crypto.priceUsd}`;
+          listCrypto.appendChild(li);
+        }
+        return
+      });
+    });
+};
+
+fetchCrypto();
