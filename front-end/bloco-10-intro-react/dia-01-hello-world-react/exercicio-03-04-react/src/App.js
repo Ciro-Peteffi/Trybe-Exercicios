c5d44716880620d43a079c14cc1a383a9bc72dd7
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-05-06 22:48:16 -0300

    adicionado modulo front-end

---
+++ front-end/bloco-10-intro-react/dia-01-hello-world-react/exercicio-03-04-react/src/App.js
@@ -0,0 +1,21 @@
+// exercicio react
+import React from 'react';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
+import './content.css'
+import './header.css'
+import './footer.css'
+
+class App extends React.Component {
+  render() {
+    return <>
+    <Header/>
+    <Content/>
+    <Footer/>
+    </>
+    
+  }
+}
+
+export default App;
