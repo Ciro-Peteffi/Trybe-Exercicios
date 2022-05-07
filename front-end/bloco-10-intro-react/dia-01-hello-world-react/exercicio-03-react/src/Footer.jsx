be1f3b86eae64cdf02f596f7ca3caa4444d20fc3
Ciro Eduardo Peteffi <92127663+Ciro-Peteffi@users.noreply.github.com>
2022-05-06 21:47:54 -0300

    Delete front-end/bloco-10-intro-react/dia-01-hello-world-react/exercicio-03-react directory

--- front-end/bloco-10-intro-react/dia-01-hello-world-react/exercicio-03-react/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Footer extends React.Component {
-    render() {
-        return <h2 className="footer">E esse é só o começo</h2>
-    }
-}
-
-export default Footer;
