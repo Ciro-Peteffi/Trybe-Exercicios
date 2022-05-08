95fd159a7dad1da3298cbf2ac5feff8b7f1f5c11
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-05-07 22:57:06 -0300

    finalizado exercicio 1 e 2

--- front-end/bloco-10-introdução-ao-react/dia-01-hello-world-react/exercise-01/src/App.js
+++ front-end/bloco-10-introdução-ao-react/dia-01-hello-world-react/exercise-01/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const arrayOfTasks = ['brushTeeth', 'makeDinner', 'cookLunch', 'goToGyn', 'studyReact', 'wakeUp', 'cleanHouse'];
+
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li className='margin'>{value}</li>
   );
+}
+
+function App() {
+  return arrayOfTasks.map(Task); 
 }
 
 export default App;
