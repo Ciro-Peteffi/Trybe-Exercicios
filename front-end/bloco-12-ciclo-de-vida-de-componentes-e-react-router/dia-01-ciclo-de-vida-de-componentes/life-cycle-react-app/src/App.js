e739ef392469014f2a5f875d21109bbe520005b3
Ciro Eduardo Peteffi <ciro.peteffi@gmail.com>
2022-05-17 20:01:25 -0300

    exercise fixation life cycle react

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/life-cycle-react-app/src/App.js
@@ -0,0 +1,29 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Profile from './components/Profile';
+import Connections from './components/Connections';
+
+class App extends React.Component {
+  constructor() {
+
+    super()
+
+    this.state = {
+      showProfile
+    }
+    
+  }
+
+  changeProfile() {
+    const { showProfile } = this.state;
+
+    this.setState({ showProfile: !showProfile });
+  }
+
+  render() {
+    // 💡 está faltando algo aqui!
+  }
+}
+
+export default App;
